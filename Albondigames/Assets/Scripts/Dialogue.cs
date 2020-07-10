1016965508ed1f3aabaf73ceff4c1d2d51c94c00
hal-9001-v <57011035+hal-9001-v@users.noreply.github.com>
2020-07-10 23:45:35 +0200

    Texts

---
+++ Albondigames/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    public string text;
+    public int delay = 0;
+
+    DialogueManager myDialogueManager;
+    void Start()
+    {
+        myDialogueManager = GameObject.FindObjectOfType<DialogueManager>();
+        triggerDialogue();
+    }
+
+    private void triggerDialogue() {
+        //Try to trigger this dialogue
+        myDialogueManager.triggerDialogue(this);
+        
+    }
+
+}
---
+++ Albondigames/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject myTextObject;
+    private TextMeshProUGUI myText;
+
+    string textToWrite;
+    int delay = 0;
+
+    void Awake()
+    {
+        //Initializing
+        myText = myTextObject.GetComponent<TextMeshProUGUI>();
+    }
+
+    public void triggerDialogue(Dialogue dialogue)
+    {
+        textToWrite = dialogue.text;
+        delay = dialogue.delay;
+
+        StartCoroutine(TypeText());
+    }
+
+    IEnumerator TypeText()
+    {
+        char[] letters = textToWrite.ToCharArray();
+
+        myText.text = "";
+
+        int counter = 0;
+        while (counter < textToWrite.Length)
+        {
+
+            myText.text += letters[counter];
+            counter++;
+
+            yield return new WaitForSeconds(0.1f);
+        }
+
+
+        yield return null;
+    }
+
+}
