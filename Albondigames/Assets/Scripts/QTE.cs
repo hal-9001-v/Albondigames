ade47e0777711ef96180afda3d3ba9fa0ec2266a
hal-9001-v <57011035+hal-9001-v@users.noreply.github.com>
2020-07-11 16:14:47 +0200

    QTE 1

---
+++ Albondigames/Assets/Scripts/QTE.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class QTE : MonoBehaviour
+{
+    public bool waitQTE;
+    public bool spamQTE;
+    public bool coordinateQTE;
+
+    public KeyCode interactionKey;
+
+    private QTESystem mySystem;
+
+    private void Start()
+    {
+        mySystem = GameObject.FindObjectOfType<QTESystem>();
+        
+    }
+
+    public void triggerQTE() {
+        if (waitQTE) {
+            mySystem.triggerWaitQTE(this);
+            return;
+        }
+
+        if (spamQTE) {
+            mySystem.triggerspamQTE(this);
+            return;
+        }
+
+        if (coordinateQTE) {
+            mySystem.triggerCoordinateQTE(this);
+            return;
+        }
+    }
+
+
+
+   
+
+
+}
---
+++ Albondigames/Assets/Scripts/QTESystem.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class QTESystem : MonoBehaviour
+{
+    public bool busy;
+
+    public void triggerWaitQTE(QTE qte)
+    {
+        if (busy)
+        {
+
+        }
+    }
+
+    public void triggerSpamQTE(QTE qte) {
+        if (busy)
+        {
+
+        }
+    }
+
+    public void triggerCoordinateQTE(QTE qte)
+    {
+        if (busy)
+        {
+
+        }
+    }
+
+
+    class ReadyState : IState
+    {
+        public void enter()
+        {
+        }
+
+        public IEnumerator Execute()
+        {
+            yield return null;
+        }
+
+        public void exit()
+        {
+        }
+    }
+    private interface IState
+    {
+        void enter();
+
+        void exit();
+
+        IEnumerator Execute();
+
+
+    }
+
+}
