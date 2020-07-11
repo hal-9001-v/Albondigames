f2a93ce82cf0cc068106113db7e68759f983d32a
hal-9001-v <57011035+hal-9001-v@users.noreply.github.com>
2020-07-11 16:58:13 +0200

    QTE

---
+++ Albondigames/Assets/Scripts/QTE.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class QTE : MonoBehaviour
+{
+    public bool waitQTE;
+    public bool spamQTE;
+    public bool coordinateQTE;
+
+    public KeyCode interactionKey;
+
+    private QTESystem mySystem;
+
+    public UnityEvent atStartActions;
+    public UnityEvent atEndActions;
+    public UnityEvent atFailureActions;
+
+    private void Start()
+    {
+        mySystem = GameObject.FindObjectOfType<QTESystem>();
+        
+    }
+
+    public void triggerQTE() {
+        if (waitQTE) {
+            mySystem.triggerWaitQTE(this);
+            return;
+        }
+
+        if (spamQTE) {
+            mySystem.triggerspamQTE(this);
+            return;
+        }
+
+        if (coordinateQTE) {
+            mySystem.triggerCoordinateQTE(this);
+            return;
+        }
+    }
+
+
+
+   
+
+
+}
