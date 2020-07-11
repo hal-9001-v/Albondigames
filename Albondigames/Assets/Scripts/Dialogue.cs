cb244f4f86acc66fa4799869a4459929b722cc41
hal-9001-v <57011035+hal-9001-v@users.noreply.github.com>
2020-07-11 13:24:46 +0200

    Dialogue v1

--- Albondigames/Assets/Scripts/Dialogue.cs
+++ Albondigames/Assets/Scripts/Dialogue.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class Dialogue : MonoBehaviour
 {
-    public string text;
-    public int delay = 0;
+    public string[] text;
+    public float delay = 0;
 
     DialogueManager myDialogueManager;
     void Start()
     {
         myDialogueManager = GameObject.FindObjectOfType<DialogueManager>();
-        triggerDialogue();
+        
     }
 
     private void triggerDialogue() {
         //Try to trigger this dialogue
         myDialogueManager.triggerDialogue(this);
         
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z)) {
+            triggerDialogue();
+        }
     }
 
 }
--- Albondigames/Assets/Scripts/DialogueManager.cs
+++ Albondigames/Assets/Scripts/DialogueManager.cs
@@ -8,41 +8,223 @@ public class DialogueManager : MonoBehaviour
     public GameObject myTextObject;
     private TextMeshProUGUI myText;
 
-    string textToWrite;
-    int delay = 0;
+    private Queue<string> linesQueue;
+    private string currentLine;
+
+    private float delay = 0;
+    public bool free = false;
+
+    public HiddenState hiddenState;
+    public TypeState typeState;
+    public IdleState idleState;
+
+    private Coroutine typingChild;
+    private bool lineIsCompleted;
 
     void Awake()
     {
         //Initializing
         myText = myTextObject.GetComponent<TextMeshProUGUI>();
+        linesQueue = new Queue<string>();
+
+        //Creating States
+        hiddenState = new HiddenState(this);
+        typeState = new TypeState(this);
+        idleState = new IdleState(this);
+
+
+    }
+
+    private void Start()
+    {
+        hiddenState.enter();
     }
 
     public void triggerDialogue(Dialogue dialogue)
     {
-        textToWrite = dialogue.text;
-        delay = dialogue.delay;
+        if (free)
+        {
+            foreach (string line in dialogue.text)
+            {
+                linesQueue.Enqueue(line);
+            }
+            delay = dialogue.delay;
 
-        StartCoroutine(TypeText());
+            hiddenState.exit();
+        }
+    }
+
+    public void showText()
+    {
+        myText.enabled = true;
+    }
+
+    public void hideText()
+    {
+        myText.enabled = false;
+    }
+
+    public bool isEmpty()
+    {
+        return linesQueue.Count == 0;
+    }
+    public void nextSentence()
+    {
+        typingChild = StartCoroutine(TypeText());
     }
 
     IEnumerator TypeText()
     {
-        char[] letters = textToWrite.ToCharArray();
+        currentLine = linesQueue.Dequeue();
+
+        lineIsCompleted = false;
 
         myText.text = "";
 
-        int counter = 0;
-        while (counter < textToWrite.Length)
+        foreach (char c in currentLine.ToCharArray())
         {
+            myText.text += c;
 
-            myText.text += letters[counter];
-            counter++;
-
-            yield return new WaitForSeconds(0.1f);
+            yield return new WaitForSeconds(delay);
         }
+
+        lineIsCompleted = true;
 
 
         yield return null;
     }
 
+    public void loadCurrentLine()
+    {
+        myText.text = currentLine;
+        StopCoroutine(typingChild);
+    }
+
+    public bool isEndOfLine()
+    {
+        return lineIsCompleted;
+    }
+
+
+    public class HiddenState : IState
+    {
+        DialogueManager dm;
+
+        public HiddenState(DialogueManager dm)
+        {
+            this.dm = dm;
+        }
+
+        //Hide textBox
+        public void enter()
+        {
+            dm.free = true;
+            dm.hideText();
+        }
+
+        //No execute is needed
+        public IEnumerator Execute()
+        {
+            yield return null;
+        }
+
+        //Display textBox and go to Type State
+        public void exit()
+        {
+            dm.free = false;
+            dm.showText();
+            dm.typeState.enter();
+
+        }
+    }
+    public class TypeState : IState
+    {
+        DialogueManager dm;
+
+        public TypeState(DialogueManager dm)
+        {
+            this.dm = dm;
+        }
+
+        //Start typing
+        public void enter()
+        {
+            dm.nextSentence();
+
+            dm.StartCoroutine(Execute());
+        }
+
+        //Check for input so full sentence will appear.
+        public IEnumerator Execute()
+        {
+            yield return null;
+
+            while (!Input.GetKeyDown(KeyCode.Z))
+            {
+                if (dm.isEndOfLine()) {
+                    break;
+                }
+                yield return null;
+            }
+
+            dm.loadCurrentLine();
+            exit();
+        }
+
+        public void exit()
+        {
+            dm.idleState.enter();
+        }
+    }
+
+    public class IdleState : IState
+    {
+        DialogueManager dm;
+
+        public IdleState(DialogueManager dm)
+        {
+            this.dm = dm;
+        }
+
+        //Start Looking for input
+        public void enter()
+        {
+            Debug.Log("ENTERING IDLE");
+            dm.StartCoroutine(Execute());
+
+        }
+
+        //Check for input so a new line will appear or dialogue will end
+        public IEnumerator Execute()
+        {
+            yield return null;
+
+            while (!Input.GetKeyDown(KeyCode.Z))
+            {
+                yield return null;
+            }
+            exit();
+
+        }
+
+        //Go to Hidden if no more lines exist or type a new line
+        public void exit()
+        {
+            if (dm.isEmpty())
+                dm.hiddenState.enter();
+            else
+                dm.typeState.enter();
+        }
+    }
+
+    private interface IState
+    {
+        void enter();
+
+        void exit();
+
+        IEnumerator Execute();
+
+
+    }
 }
