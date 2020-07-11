e10ea7faec7a88456d34a340b3601e072b0c2267
dexaxi <dexaxi12@gmail.com>
2020-07-11 20:39:42 +0200

    ANIMACIONES HASTA LA POLLA ME TENEIS CON VUESTRAS ANIMACIONES DE MIERdA Y EL PUTO ANIMATOR DE LOS COJONES TOMA VICENTE QUE TE LO PASES BIEN MERGEANDO COMEME LA POLLA YA ME TIENES HASTA LA POLLA

---
+++ Albondigames/Assets/Scripts/Animations/Anim.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anim : MonoBehaviour
+{
+    Animator animator;
+    PlayerController player;
+    void Awake(){
+
+        animator = FindObjectOfType<Animator>();
+        player = FindObjectOfType<PlayerController>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("Moving",player.isWalking);//
+        animator.SetBool("Grounded", player.isGrounded);//
+        animator.SetBool("Burping", player.isBurping);//
+        animator.SetBool("Running", player.isRunning);//
+        animator.SetBool("Punching", player.isPunching);//
+        animator.SetBool("Peeing", player.isPeeing);
+        animator.SetBool("Falling", player.isFalling);
+        animator.SetBool("Lieing", player.isLieing);
+        animator.SetBool("Lied", player.isLed);
+        animator.SetBool("GettingUp", player.isGettingUp);
+        animator.SetBool("Climbing", player.isClimbing | player.isMoving);
+        animator.SetBool("IdleClimbing", player.canClimb);//
+    }
+}
--- Albondigames/Assets/Scripts/Player/PlayerController.cs
+++ Albondigames/Assets/Scripts/Player/PlayerController.cs
@@ -13,12 +13,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private LayerMask ladm;
     public PlayerStats ps;
     public Punch punch;
+    private SpriteRenderer spr;
     //Character attributes
     private float MOVEMENT_SPEED = 10f;
     private float CLIMBING_SPEED = 10f;
     private float xtraheight = .1f;
     private float midAirControl = 5f;
-    float jumpVelocity = 12f;
+    float jumpVelocity = 20f;
     private bool inmune = false;
     bool b = false;
 
@@ -27,6 +28,19 @@ public class PlayerController : MonoBehaviour
     //GAME CONTROL BOOLEANS
     // MOVEMENT
     public bool canClimb = false;
+    public bool isMoving = false;
+    public bool isGrounded = true;
+    public bool isPunching = false;
+    public bool isBurping = false;
+    public bool isRunning = false;
+    public bool isPeeing = false;
+    public bool isFalling = false;
+    public bool isLieing = false;
+    public bool isLed = false;
+    public bool isGettingUp = false;
+    public bool isClimbing = false;
+    public bool isWalking = false;
+   
     // LEVEL 1
     public bool jToMove = false; //
     public bool canMove = true; //
@@ -46,6 +60,7 @@ public class PlayerController : MonoBehaviour
     private void Awake(){
     rb2d = GetComponent<Rigidbody2D>();
     bx2d = GetComponent<BoxCollider2D>();
+    spr = GetComponent<SpriteRenderer>();
     ps = FindObjectOfType<PlayerStats>();
     punch = FindObjectOfType<Punch>();
     initVars();
@@ -55,6 +70,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isRunning)  {MOVEMENT_SPEED = 15f;} else {MOVEMENT_SPEED = 10f;}
+        AnimationHandler();
         if (!canClimb){
             if(!invertedControls) {
             
@@ -146,7 +163,7 @@ public class PlayerController : MonoBehaviour
     }
 
     public void Burp(){
-   if(Input.GetKeyDown(KeyCode.E)) {
+   if(Input.GetKeyDown(KeyCode.E) && isGrounded) {
             float r = Random.Range(1, 5);
                Debug.Log(r);
             switch (r) {
@@ -166,7 +183,20 @@ public class PlayerController : MonoBehaviour
                 SoundManager.PlaySound(SoundManager.Sound.eructo4, 0.1f);
                 break;      
             }
+            StartCoroutine(BurpWait());
+
         }
+
+    }
+
+    IEnumerator BurpWait()
+    {
+       isBurping= true;
+       canMove = false;
+        yield return new WaitForSeconds(0.75f);
+        canMove = true;
+        isBurping= false;
+        inmune = false;
     }
 
     private bool IsGrounded(){
@@ -209,59 +239,87 @@ public class PlayerController : MonoBehaviour
     StartCoroutine(PunchWait());
    }
    
+   void AnimationHandler(){
+
+       if(rb2d.velocity.x != 0) {
+           isMoving = true;
+       } else { isMoving = false;}
+if(rb2d.velocity.y != 0) {
+           isClimbing = true;
+       } else { isClimbing = false;}
+    if(IsGrounded()) { isGrounded = true;} else {isGrounded = false;}
+   
+   }
  IEnumerator PunchWait()
     {
         b = false;
-        
+        isPunching = true;
+        canMove = false;
         yield return new WaitForSeconds(0.75f);
+        canMove = true;
+        isPunching = false;
         inmune = false;
-    punch.SetActive(b);
+        punch.SetActive(b);
     }
 
 
     private void HandleMovement(){
-        rb2d.gravityScale = 2;
+        rb2d.gravityScale = 3;
 
-        if (canPunch && Input.GetKeyDown(KeyCode.Q)){
+        if (Input.GetKey(KeyCode.LeftShift)){
+            isRunning = true;
+        } else {isRunning = false;}
+        if (canPunch && Input.GetKeyDown(KeyCode.Q) && isGrounded){
        FalconPunch();
         }
         Burp();
         if (Input.GetKey(KeyCode.A) && canMove) {
+            spr.flipX = true;
             if(IsGrounded()){
+                isWalking=true;
             rb2d.velocity = new Vector2( -MOVEMENT_SPEED, rb2d.velocity.y);
             } else {
             rb2d.velocity += new Vector2( -MOVEMENT_SPEED*midAirControl* Time.deltaTime, 0);
             rb2d.velocity = new Vector2(Mathf.Clamp(rb2d.velocity.x, -MOVEMENT_SPEED, +MOVEMENT_SPEED), rb2d.velocity.y);
             }
         } else if(Input.GetKey(KeyCode.D) && !jToMove && canMove)  {
-                        if(IsGrounded()){
+             spr.flipX = false;
+            if(IsGrounded()){
+            isWalking = true;
             rb2d.velocity = new Vector2( MOVEMENT_SPEED, rb2d.velocity.y);
             } else {
             rb2d.velocity += new Vector2( +MOVEMENT_SPEED *midAirControl * Time.deltaTime, 0);
             rb2d.velocity = new Vector2(Mathf.Clamp(rb2d.velocity.x, -MOVEMENT_SPEED, +MOVEMENT_SPEED), rb2d.velocity.y);
             }
         } else if (Input.GetKey(KeyCode.J) && jToMove && canMove) {
+            spr.flipX = false;
                if(IsGrounded()){
-            rb2d.velocity = new Vector2( MOVEMENT_SPEED, rb2d.velocity.y);
+                   isWalking = true;
+                    rb2d.velocity = new Vector2( MOVEMENT_SPEED, rb2d.velocity.y);
             } else {
             rb2d.velocity += new Vector2( +MOVEMENT_SPEED *midAirControl * Time.deltaTime, 0);
             rb2d.velocity = new Vector2(Mathf.Clamp(rb2d.velocity.x, -MOVEMENT_SPEED, +MOVEMENT_SPEED), rb2d.velocity.y);
             }
         }else {
             if(IsGrounded()) {
+                isWalking = false;
                 rb2d.velocity = new Vector2(0, rb2d.velocity.y);
             }
         }
     }
 
     private void HandleInvertedMovement(){
-      rb2d.gravityScale = 2;
+      rb2d.gravityScale = 3;
 
-    if (canPunch&& Input.GetKeyDown(KeyCode.Q)){
-           //
+        if (Input.GetKey(KeyCode.LeftShift)){
+            isRunning = true;
+        } else {isRunning = false;}
+        if (canPunch && Input.GetKeyDown(KeyCode.Q) && isGrounded){
+       FalconPunch();
         }
         Burp();
         if (Input.GetKey(KeyCode.D) && canMove) {
+            spr.flipX = true;
             if(IsGrounded()){
             rb2d.velocity = new Vector2( -MOVEMENT_SPEED, rb2d.velocity.y);
             } else {
@@ -269,6 +327,7 @@ public class PlayerController : MonoBehaviour
             rb2d.velocity = new Vector2(Mathf.Clamp(rb2d.velocity.x, -MOVEMENT_SPEED, +MOVEMENT_SPEED), rb2d.velocity.y);
             }
         } else if(Input.GetKey(KeyCode.A) && !jToMove && canMove)  {
+                        spr.flipX = false;
                         if(IsGrounded()){
             rb2d.velocity = new Vector2( MOVEMENT_SPEED, rb2d.velocity.y);
             } else {
@@ -287,7 +346,6 @@ public class PlayerController : MonoBehaviour
                 rb2d.velocity = new Vector2(0, rb2d.velocity.y);
             }
         }
-
 
     }
 
@@ -315,10 +373,11 @@ else if (Input.GetKeyDown(KeyCode.S)){
 }
 
 
+
+
 }
 
     private void HandleInvertedClimbMovement(){
-
 
 rb2d.gravityScale = 0;
 if(Input.GetKeyDown(KeyCode.S)){
@@ -341,8 +400,9 @@ else if (Input.GetKeyDown(KeyCode.W)){
 
 }
 
+    }
 
 }
 
 
-}
+
