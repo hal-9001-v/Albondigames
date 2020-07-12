d6897a0dd15acc488ab5aedbf4c6257dba724c21
hal-9001-v <57011035+hal-9001-v@users.noreply.github.com>
2020-07-12 20:13:40 +0200

    update

---
+++ Albondigames/Assets/Prefabs/LinePasser.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LinePasser : Triggerable
+{
+    public override void trigger()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("Changing Room");
+
+    }
+}
